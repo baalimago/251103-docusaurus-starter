fb4158942c34bacd503040e877d936ca19e09e1b
Lorentz Kinde <l.kinde94@outlook.com>
2025-11-13 13:55:32 +0200

    Update index.js 25-11-13-0

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}. This is an update.`}
+      title={`Hello from ${siteConfig.title}. This is an update. 25-11-13-0`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
